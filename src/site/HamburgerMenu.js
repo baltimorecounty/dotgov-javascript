86fd162db2837e18e25904e7342e56c177f78d5f
Travis H <theilman@baltimorecountymd.gov>
2019-09-09 13:50:16 -0400

    Updating script and js.

--- src/site/HamburgerMenu.js
+++ src/site/HamburgerMenu.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
   var contentWrapper, menu, menuToggle, toggleButton, closeButton;
 
   contentWrapper = document.getElementById("content");
-  menu = document.getElementById("slide-menu");
-  menuToggle = document.getElementById("menu-toggle");
-  toggleButton = document.getElementById("menu-trigger-btn");
-  closeButton = document.getElementById("cntnt-ovrly");
+  menu = document.getElementById("dg_slide-menu");
+  menuToggle = document.getElementById("dg_menu-toggle");
+  toggleButton = document.getElementById("dg_menu-trigger-btn");
+  closeButton = document.getElementById("dg_cntnt-ovrly");
 
   toggleButton.addEventListener("click", function(c) {
     c.preventDefault();
