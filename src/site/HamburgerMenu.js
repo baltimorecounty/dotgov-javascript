2642768d9aac3105d66a0f29a0841de6fb7a7bdb
Travis H <theilman@baltimorecountymd.gov>
2019-09-09 09:06:35 -0400

    Updated naming conventions.

--- src/site/HamburgerMenu.js
+++ src/site/HamburgerMenu.js
@@ -2,20 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
   var contentWrapper, menu, menuToggle, toggleButton, closeButton;
 
   contentWrapper = document.getElementById("content");
-  menu = document.getElementById("slide-menu");
-  menuToggle = document.getElementById("menu-toggle");
-  toggleButton = document.getElementById("menu-trigger-btn");
-  closeButton = document.getElementById("cntnt-ovrly");
+  menu = document.getElementById("dg_slide-menu");
+  menuToggle = document.getElementById("dg_menu-toggle");
+  toggleButton = document.getElementById("dg_menu-trigger-btn");
+  closeButton = document.getElementById("dg_cntnt-ovrly");
 
   toggleButton.addEventListener("click", function(c) {
     c.preventDefault();
-    closeButton.classList.toggle("menu-active");
-    contentWrapper.classList.toggle("menu-active");
-    menu.classList.toggle("menu-active");
+    closeButton.classList.toggle("dg_menu-active");
+    contentWrapper.classList.toggle("dg_menu-active");
+    menu.classList.toggle("dg_menu-active");
   });
 
   closeButton.addEventListener("click", function(c) {
     c.preventDefault();
-    menu.classList.toggle("menu-active");
+    menu.classList.toggle("dg_menu-active");
   });
 });
