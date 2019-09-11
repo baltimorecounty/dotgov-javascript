4e3f7c3a936f61ed95489b22e1e4c4c2a2fbded4
Travis H <theilman@baltimorecountymd.gov>
2019-09-11 11:45:58 -0400

    Updated dg_slide-menu to dg_site-menu.

--- src/site/HamburgerMenu.js
+++ src/site/HamburgerMenu.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var contentWrapper, menu, menuToggle, toggleButton, closeButton;
 
   contentWrapper = document.getElementById("content");
-  menu = document.getElementById("dg_slide-menu");
+  menu = document.getElementById("dg_site-menu");
   menuToggle = document.getElementById("dg_menu-toggle");
   toggleButton = document.getElementById("dg_menu-trigger-btn");
   closeButton = document.getElementById("dg_cntnt-ovrly");
