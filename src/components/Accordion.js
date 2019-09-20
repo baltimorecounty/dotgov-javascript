87d0c3a45ec126acf63757c813685a0508d12515
tmccoy <tmccoy@baltimorecountymd.gov>
2019-09-20 08:03:07 -0400

    Setup Project

---
+++ src/components/Accordion.js
@@ -0,0 +1,30 @@
+function BuildHTML() {
+  var div = document.createElement("div");
+  div.style.backgroundColor = "red";
+  div.width = "300px";
+  div.innerHTML = "Hello World";
+  alert("Huh?");
+}
+
+function BuildMenus() {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+      this.classList.toggle("active");
+
+      /* Toggle between hiding and showing the active panel */
+      var panel = this.nextElementSibling;
+      if (panel.style.display === "block") {
+        panel.style.display = "none";
+      } else {
+        panel.style.display = "block";
+      }
+    });
+  }
+}
+
+BuildHTML();
