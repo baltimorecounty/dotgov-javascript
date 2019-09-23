6bc3e041d6fe3c0696b9bbb22113bafdee0162c1
martypowell <jpowel25@gmail.com>
2019-09-23 11:56:03 -0400

    fixed formatting

--- src/components/StepList.fixture.js
+++ src/components/StepList.fixture.js
@@ -3,27 +3,21 @@ export default `<div class="dg_step-list collapsed">
     <ol class="dg_step-list__list">
         <li>
             <h3>Step 1</h3>
-            <button class="dg_step-list__toggle-btn" type="button">
-                Show
-            </button>
+            <button class="dg_step-list__toggle-btn" type="button">Show</button>
             <div class="dg_step-list__details">
                 <p>Some description text goes here.</p>
             </div>
         </li>
         <li>
             <h3>Step 2</h3>
-            <button class="dg_step-list__toggle-btn" type="button">
-                Hide
-            </button>
+            <button class="dg_step-list__toggle-btn" type="button">Hide</button>
             <div class="dg_step-list__details">
                 <p>Some description text goes here.</p>
             </div>
         </li>
         <li>
             <h3>Step 3</h3>
-            <button class="dg_step-list__toggle-btn" type="button">
-                Show
-            </button>
+            <button class="dg_step-list__toggle-btn" type="button">Show</button>
             <div class="dg_step-list__details">
                 <p>Some description text goes here.</p>
             </div>
