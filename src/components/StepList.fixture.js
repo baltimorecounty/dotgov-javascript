3af4555ad4e384fd2af8d96004317f40937a02d1
martypowell <jpowel25@gmail.com>
2019-10-17 14:09:55 -0400

    adjusted fixture so we coudl test generically

--- src/components/StepList.fixture.js
+++ src/components/StepList.fixture.js
@@ -119,40 +119,17 @@ export default (type = null) => `
             <span class="dg_step-list__toggle-btn__btn-text">Show</span>
         </button>
         <div class="dg_step-list__details" id="registration-panel-2" aria-labelledby="registration-step-2">
-            <p>
-            Have an inspection performed by a
-            <a title="Find state licensed home inspectors." href="http://www.dllr.state.md.us/pq/">state-licensed home inspector</a>. The inspector must complete an
-            <a target="_blank" onclick="window.open('http://resources.baltimorecountymd.gov/Documents/Permits/rental_registration/5inspectionsheet.pdf', '_blank', 'toolbar=yes,menubar=yes,location=yes,scrollbars=yes,resizable=yes,status=yes,width=800,height=600'); return false;" title="Download inspection sheet filled out by an inspector. (opens new window)" href="http://resources.baltimorecountymd.gov/Documents/Permits/rental_registration/5inspectionsheet.pdf">Inspection Sheet</a>
-            (PDF) for each unit you intend to rent.
-            </p>
+            <p>Step 2 details.</p>
         </div>
         </li>
         <li class="dg_step-list__list-section" id="registration-step-3">
-        <button class="dg_step-list__toggle-btn" aria-expanded="false" aria-controls="registration-panel-3" type="button">
-            <span class="dg_step-list__toggle-btn__title">Step 3: Gather Required Documentation</span>
-            <span class="dg_step-list__toggle-btn__btn-text">Show</span>
-        </button>
-        <div class="dg_step-list__details" id="registration-panel-3" aria-labelledby="registration-step-3">
-            <p>You must provide the following with your application:</p>
-            <ul>
-            <li>
-                The
-                <a target="_blank" onclick="window.open('http://resources.baltimorecountymd.gov/Documents/Permits/rental_registration/5inspectionsheet.pdf', '_blank', 'toolbar=yes,menubar=yes,location=yes,scrollbars=yes,resizable=yes,status=yes,width=800,height=600'); return false;" title="Download the inspection sheet to be filled out by inspector. (opens new window)" href="http://resources.baltimorecountymd.gov/Documents/Permits/rental_registration/5inspectionsheet.pdf">Inspection Sheet</a>
-                (PDF) completed by a
-                <a title="Find state-licensed home inspectors." href="http://www.dllr.state.md.us/pq/">licensed home inspector</a>
-                in Step 2 above
-            </li>
-            <li>
-                A
-                <a target="_blank" onclick="window.open('http://resources.baltimorecountymd.gov/Documents/Permits/rental_registration/alarmverificationform.pdf', '_blank', 'toolbar=yes,menubar=yes,location=yes,scrollbars=yes,resizable=yes,status=yes,width=800,height=600'); return false;" title="Download form to verify carbon monoxide alarm is installed. (opens new window)" href="http://resources.baltimorecountymd.gov/Documents/Permits/rental_registration/alarmverificationform.pdf">Carbon Monoxide Alarm Verification</a>
-                (PDF)
-            </li>
-            <li>
-                If the home was built before 1978, a lead inspection
-                certificate
-            </li>
-            </ul>
-        </div>
+            <button class="dg_step-list__toggle-btn" aria-expanded="false" aria-controls="registration-panel-3" type="button">
+                <span class="dg_step-list__toggle-btn__title">Step 3: Gather Required Documentation</span>
+                <span class="dg_step-list__toggle-btn__btn-text">Show</span>
+            </button>
+            <div class="dg_step-list__details" id="registration-panel-3" aria-labelledby="registration-step-3">
+                <p>Step 3 details.</p>
+            </div>
         </li>
     </ol>
     </div>
