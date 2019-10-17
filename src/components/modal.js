bea6277678dab47125ba271a9fa646e2db9b6ee2
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-10-17 11:11:03 -0400

    using eventlistener

--- src/components/modal.js
+++ src/components/modal.js
@@ -5,14 +5,20 @@ import A11yDialog from "a11y-dialog";
  * @param {string} contentId
  * @param {function} onShow
  */
-
-const init = (modalClass, onShow = () => {}) => {
-  const modalElms = document.querySelectorAll(`[class*=${modalClass}]`);
-  modalElms.forEach(modalElm => {
-    const dialog = modalElm;
-    const modal = new A11yDialog(dialog);
-    modal.on("show", onShow);
-  });
-};
-
-export { init };
+const modalElms = document.querySelectorAll(`[class*=${"g_modal"}]`);
+document.addEventListener(
+  "DOMContentLoaded",
+  () => {
+    console.log("Dom Fully loaded");
+    console.log(modalElms);
+    const modalClass = "dg_modal";
+    //const init = (g_modal, onShow = () => {}) => {
+    modalElms.forEach(modalElm => {
+      const dialog = modalElm;
+      const modal = new A11yDialog(dialog);
+      modal.on("show");
+    });
+    // };
+  },
+  false
+);
