0627743476d1baf909b502bbe7d58e2c6b02e269
martypowell <jpowel25@gmail.com>
2019-10-17 11:28:06 -0400

    enabled modals on page load

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,24 +1,12 @@
 import A11yDialog from "a11y-dialog";
 
-/**
- *@param {string } modalClass
- * @param {string} contentId
- * @param {function} onShow
- */
-const modalElms = document.querySelectorAll(`[class*=${"g_modal"}]`);
-document.addEventListener(
-  "DOMContentLoaded",
-  () => {
-    console.log("Dom Fully loaded");
-    console.log(modalElms);
-    const modalClass = "dg_modal";
-    //const init = (g_modal, onShow = () => {}) => {
-    modalElms.forEach(modalElm => {
-      const dialog = modalElm;
-      const modal = new A11yDialog(dialog);
-      modal.on("show");
-    });
-    // };
-  },
-  false
-);
+const initializeModal = modalElm => {
+  const dialog = modalElm;
+  new A11yDialog(dialog);
+};
+
+document.addEventListener("DOMContentLoaded", onContentLoadedEvent => {
+  const modalClass = "dg_modal";
+  const modalElms = document.querySelectorAll(`[class*=${modalClass}]`);
+  modalElms.forEach(initializeModal);
+});
