5f069b3e7c84e3f9dc7e1b9ba985caf24c06ed17
martypowell <jpowel25@gmail.com>
2019-10-21 14:19:45 -0400

    imported w3 logic and and click handlers for use with web services

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,12 +1,40 @@
-import A11yDialog from "a11y-dialog";
+import "./w3-dialog";
 
-const initializeModal = modalElm => {
-  const dialog = modalElm;
-  new A11yDialog(dialog);
+const handleDocumentClick = clickEvent => {
+  const { classList: targetClassList } = clickEvent.target;
+  // If the clicked element doesn't have the right selector, bail
+  const isModalOpenButtonClick = targetClassList.contains(
+    "dg_modal__open-button"
+  );
+  const isModalCloseButtonClick = targetClassList.contains(
+    "dg_modal__close-button"
+  );
+
+  if (isModalOpenButtonClick) {
+    handleModalOpenButtonClick(clickEvent);
+  } else if (isModalCloseButtonClick) {
+    handleModalCloseButtonClick(clickEvent);
+  } else {
+    return;
+  }
 };
 
-document.addEventListener("DOMContentLoaded", onContentLoadedEvent => {
-  const modalClass = "dg_modal";
-  const modalElms = document.querySelectorAll(`[class*=${modalClass}]`);
-  modalElms.forEach(initializeModal);
-});
+/**
+ * Closes the closest modal
+ * @param {*} clickEvent
+ */
+const handleModalCloseButtonClick = clickEvent => {
+  window.closeDialog(clickEvent.target);
+};
+
+/**
+ * Opens the modal that matches it's id to data-target attribute of the open button
+ * @param {*} clickEvent
+ */
+const handleModalOpenButtonClick = clickEvent => {
+  const modalButtonElm = clickEvent.target;
+  const targetModalId = modalButtonElm.getAttribute("data-target");
+  window.openDialog(targetModalId, modalButtonElm);
+};
+
+document.addEventListener("click", handleDocumentClick, false);
