26f79d73b312008078809d24f177f425ff335659
martypowell <jpowel25@gmail.com>
2020-01-16 11:42:22 -0500

    added nav to site js and rebuilt

--- src/site/Site.js
+++ src/site/Site.js
@@ -1,9 +1,8 @@
 /** All files required on each page of the dotgov website */
 
 /** Site Wide Components */
+
 import "../components/Accordion";
 import "../components/Modal";
 import "../components/StepList";
-
-/** Site Specific */
-// import "./SiteNavigation"; will be enabled when ready
+import "./SiteNavigation";
