7d6fe1a3739ebe7e3f32c8379fbf6dfccdf34166
Travis H <theilman@baltimorecountymd.gov>
2020-01-23 10:37:06 -0500

    Updated outdated script.

--- src/template/SubpageTabs.js
+++ src/template/SubpageTabs.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
   var menu, menuToggle, toggleButton;
 
-  menu = document.getElementById("tabs");
+  menu = document.getElementById("dg_tabs");
   menuToggle = document.getElementById("dg_menu-toggle");
   toggleButton = document.getElementById("dg_menu-trigger-btn");
 
