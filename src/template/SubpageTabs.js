e0bd4e0f29281b3d3e2edc885e9dd1cc386acf37
Travis H <theilman@baltimorecountymd.gov>
2020-01-31 13:52:32 -0500

    Removed unused variable, menuToggle.

--- src/template/SubpageTabs.js
+++ src/template/SubpageTabs.js
@@ -1,8 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-  var menu, menuToggle, toggleButton;
+  var menu, toggleButton;
 
   menu = document.getElementById("dg_tabs");
-  menuToggle = document.getElementById("dg_menu-toggle");
   toggleButton = document.getElementById("dg_menu-trigger-btn");
 
   toggleButton.addEventListener("click", function(clickEvent) {
