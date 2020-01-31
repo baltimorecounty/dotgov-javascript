f83d1f627e925f1db058afa7e98077470aac336d
martypowell <jpowel25@gmail.com>
2020-01-31 13:54:42 -0500

    updated search results stcript to check for empy search term

--- src/page-specific/SearchResults.js
+++ src/page-specific/SearchResults.js
@@ -2,6 +2,22 @@ import { Urls } from "@baltimorecounty/javascript-utilities";
 
 const { GetParameterByName } = Urls;
 
-const searchTerm = GetParameterByName("q");
+/** Shows a friendly message to the user if no search term is "searched" */
+const handleEmptySearch = () => {
+  const searchTerm = GetParameterByName("q");
 
-console.log(searchTerm);
+  if (!searchTerm) {
+    const mainContentElm = document.getElementById("dg_main-content");
+    const noResultsElm = document.createElement("p");
+    noResultsElm.innerHTML =
+      "The search term you used was empty, please try again using the site search.";
+    mainContentElm.insertBefore(noResultsElm, mainContentElm.firstChild);
+  }
+};
+
+const onDocumentReady = () => {
+  handleEmptySearch();
+};
+
+/** Handler when the DOM is fully loaded */
+document.addEventListener("DOMContentLoaded", onDocumentReady);
