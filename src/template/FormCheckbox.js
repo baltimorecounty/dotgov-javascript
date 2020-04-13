1719afda65b83e33c1d56824033bd03badb3a887
martypowell <jpowel25@gmail.com>
2020-04-13 13:40:13 -0400

    removed unused file

--- src/template/FormCheckbox.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const checkboxContainer = document.getElementsByClassName("seFieldCell seFieldCellHorizontal dg_checkbox");
-    const checkboxLabel = document.getElementById("accepterms-label");
-    checkboxContainer.appendChild(checkboxLabel);
-  });
