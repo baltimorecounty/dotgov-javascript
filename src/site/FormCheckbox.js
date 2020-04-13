44bdf1aab99f32f30ffc39da5eda9f8d4088e19e
theilman7 <34519645+theilman7@users.noreply.github.com>
2020-04-13 10:48:37 -0400

    Cleaned up repo.

--- src/site/FormCheckbox.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const checkboxContainer = document.getElementsByClassName("seFieldCell seFieldCellHorizontal dg_checkbox");
-    const checkboxLabel = document.getElementsByClassName("dg_label dg_checkbox-label");
-    checkboxContainer.appendChild(checkboxLabel);
-});
-  
