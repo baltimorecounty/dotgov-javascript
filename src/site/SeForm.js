420e3c6304c26b32cb882c97d1bd0f7aa732016d
martypowell <jpowel25@gmail.com>
2020-04-13 13:33:01 -0400

    added move checkbox logic

--- src/site/SeForm.js
+++ src/site/SeForm.js
@@ -1,12 +1,44 @@
+import { GetFirstElementOrDefault } from "../utilities/dom.utilities";
+
 const GenerateCheckboxes = () => {
-  document.querySelectorAll(".seCheckbox").forEach(function(inputElm) {
+  document.querySelectorAll(".seCheckbox").forEach(function (inputElm) {
     inputElm.closest("div").classList.add("dg_checkbox");
   });
 };
 
 const GenerateRadioButtons = () => {
-  document.querySelectorAll(".seRadio").forEach(function(inputElm) {
+  document.querySelectorAll(".seRadio").forEach(function (inputElm) {
     inputElm.closest("div").classList.add("dg_radio");
+  });
+};
+
+/**
+ * Moves Checkbox fields (Ex Disclaimer)so that they are styled like other se form elements.
+ */
+const MoveCheckboxes = () => {
+  var horizontalCheckBoxElms = document.querySelectorAll(
+    ".SEAFGroupHorizontal"
+  );
+
+  [...horizontalCheckBoxElms].forEach((elm) => {
+    const fieldContainerElm = GetFirstElementOrDefault(
+      elm,
+      ".seFieldCellHorizontal"
+    );
+    const labelContainerElm = GetFirstElementOrDefault(
+      elm,
+      ".seLabelCellHorizontal"
+    );
+    const shouldBeMoved = fieldContainerElm && labelContainerElm;
+
+    if (shouldBeMoved) {
+      //   fieldContainerElm.closest("div").classList.add("dg_checkbox");
+      const labelElm = GetFirstElementOrDefault(labelContainerElm, "label");
+
+      labelElm.classList.add("seCheckboxLabel");
+
+      fieldContainerElm.appendChild(labelElm);
+    }
   });
 };
 
@@ -14,4 +46,5 @@ const GenerateRadioButtons = () => {
 document.addEventListener("DOMContentLoaded", () => {
   GenerateCheckboxes();
   GenerateRadioButtons();
+  MoveCheckboxes();
 });
