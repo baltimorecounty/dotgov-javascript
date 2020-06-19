062e4494b8a4dfdec32e9c9f2d8288a76d3635ec
martypowell <jpowel25@gmail.com>
2020-06-19 09:00:33 -0400

    check for existence of dg tabs

--- src/template/SubpageTabs.js
+++ src/template/SubpageTabs.js
@@ -1,9 +1,10 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const menu = document.getElementById("dg_tabs");
   const toggleButton = document.getElementById("dg_menu-trigger-btn");
-
-  toggleButton.addEventListener("click", function(clickEvent) {
-    clickEvent.preventDefault();
-    menu.classList.toggle("open");
-  });
+  if (toggleButton) {
+    toggleButton.addEventListener("click", function (clickEvent) {
+      clickEvent.preventDefault();
+      menu.classList.toggle("open");
+    });
+  }
 });
