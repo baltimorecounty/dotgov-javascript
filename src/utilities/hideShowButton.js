9eddff711a37f0c1f6aec6845b7dcf08eccfa341
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-08-04 10:28:21 -0400

    updated comment

--- src/utilities/hideShowButton.js
+++ src/utilities/hideShowButton.js
@@ -3,7 +3,7 @@ function hideButton() {
   var msie = ua.indexOf("MSIE ");
 
   if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
-    // If Internet Explorer, return version number
+    // If Internet Explorer, hide button
     var showMoreLessButton = document.getElementById("toggle-btn");
     showMoreLessButton.style.display = "none";
   }
