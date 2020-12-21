f749e1e80a4ad9fd04a55d71b16fdfe2386b594c
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-12-21 14:01:15 -0500

    check if data table already exists

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -1,36 +1,49 @@
 //Any table with an ID of responsive-main-table will have this run against it to create the responsive data table
 const CreateDataTable = () => {
-  $("#responsive-main-table").DataTable({
-    info: false,
-    paging: false,
-    bFilter: false,
-    processing: true,
-    ordering: true,
-    responsive: true,
-    autoWidth: false,
-    order: [[1, "asc"]],
-    columnDefs: [
-      {
-        targets: 0,
-        orderable: false,
-      },
-    ],
-  });
+  if (!$.fn.DataTable.isDataTable("#responsive-main-table")) {
+    $("#responsive-main-table").DataTable({
+      info: false,
+      paging: false,
+      bFilter: false,
+      processing: true,
+      ordering: true,
+      responsive: true,
+      autoWidth: false,
+      order: [[1, "asc"]],
+      columnDefs: [
+        {
+          targets: 0,
+          orderable: false,
+        },
+      ],
+    });
+  }
 };
 
-//Hides any columns without header text. These headers are typically image or icon headers and not needed for the responsive table.
-// const HideHeaders = () => {
-//   var responsiveTable = $("#responsive-main-table").DataTable();
-//   var columns = responsiveTable.columns().header().length;
-//   var w = window.innerWidth;
+// function dynamicallyLoadScript(callback) {
+//   // Adding the script tag to the head as suggested before
+//   var head = document.head;
+//   var script1 = document.createElement("script"); // create a script DOM node
+//   script1.src =
+//     "https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"; // set its src to the provided URL
 
-//   for (i = 0; i < columns; i++) {
-//     var header = responsiveTable.column(i).header();
-//     if (header.innerHTML === "") {
-//       responsiveTable.column(i).visible(w > 767);
-//     }
-//   }
-// };
+//   var script2 = document.createElement("script"); // create a script DOM node
+//   script2.src =
+//     "https://cdn.datatables.net/responsive/2.2.6/js/dataTables.responsive.min.js"; // set its src to the provided URL
+
+//   // Then bind the event to the callback function.
+//   // There are several events for cross browser compatibility.
+//   script1.onreadystatechange = callback;
+//   script1.onload = callback;
+
+//   script2.onreadystatechange = callback;
+//   script2.onload = callback;
+
+//   // Fire the loading
+//   head.appendChild(script1);
+//   head.appendChild(script2);
+// }
+
+// //loadScript(CreateDataTable());
 
 window.addEventListener("load", CreateDataTable);
-
