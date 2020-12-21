d69ddbd946a5aa65d488eb838bb7a80118cfc75e
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2020-12-21 10:22:09 -0500

    removed hide headers

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -1,7 +1,5 @@
 //Any table with an ID of responsive-main-table will have this run against it to create the responsive data table
-const applyDataTable = () => {
-  $("#responsive-main-table").removeClass("table");
-  
+const CreateDataTable = () => {
   $("#responsive-main-table").DataTable({
     info: false,
     paging: false,
@@ -18,44 +16,21 @@ const applyDataTable = () => {
       },
     ],
   });
-
-  $("#responsive-main-table").addClass("table");
 };
 
 //Hides any columns without header text. These headers are typically image or icon headers and not needed for the responsive table.
-const HideHeaders = () => {
-  var $responsiveTable = $("#responsive-main-table").DataTable({
-    info: false,
-    paging: false,
-    bFilter: false,
-    processing: true,
-    ordering: true,
-    responsive: true,
-    autoWidth: false,
-    order: [[1, "asc"]],
-    columnDefs: [
-      {
-        targets: 0,
-        orderable: false,
-      },
-    ],
-  });
-  var columns = $responsiveTable.columns().count();
-  var w = window.innerWidth;
+// const HideHeaders = () => {
+//   var responsiveTable = $("#responsive-main-table").DataTable();
+//   var columns = responsiveTable.columns().header().length;
+//   var w = window.innerWidth;
 
-  for (i = 0; i < columns; i++) {
-    var header = $responsiveTable.column(i).header();
-    if (header.innerHTML === "") {
-      $responsiveTable.column(i).visible(w > 767);
-      Vas;
-    }
-  }
-};
+//   for (i = 0; i < columns; i++) {
+//     var header = responsiveTable.column(i).header();
+//     if (header.innerHTML === "") {
+//       responsiveTable.column(i).visible(w > 767);
+//     }
+//   }
+// };
 
-window.addEventListener("load", applyDataTable);
+window.addEventListener("load", CreateDataTable);
 
-//trigger upon pageload
-//document.addEventListener("DOMContentLoaded", HideHeaders);
-
-//trigger upon screen resize
-//window.addEventListener("resize", HideHeaders);
