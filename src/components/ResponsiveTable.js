c70d93783f40900d5b408dc3bdbe56b53ca06075
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-11 17:52:07 -0500

    Converted responsive table to css class from ID

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -1,7 +1,7 @@
 //Any table with an ID of responsive-main-table will have this run against it to create the responsive data table
 const CreateDataTable = () => {
-  if (!$.fn.DataTable.isDataTable("#responsive-main-table")) {
-    $("#responsive-main-table").DataTable({
+  if (!$.fn.DataTable.isDataTable(".responsive-main-table")) {
+    $(".responsive-main-table").DataTable({
       info: false,
       paging: false,
       bFilter: false,
@@ -16,7 +16,7 @@ const CreateDataTable = () => {
 };
 
 const CheckForBlankHeader = () => {
-  $("#responsive-main-table th").each(function (index) {
+  $(".responsive-main-table th").each(function (index) {
     var Header = $(this);
     if (!Header.text()) {
       Header.removeClass("sorting").addClass("sorting_disabled");
