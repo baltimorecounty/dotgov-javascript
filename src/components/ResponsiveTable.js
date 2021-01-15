c374e0bc1b23709320aa6271dbca1a2d6ee7e388
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-15 09:01:11 -0500

    added sort order class and check

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -11,7 +11,7 @@ const CreateDataTable = () => {
       ordering: true,
       responsive: true,
       autoWidth: false,
-      order: [[0, "asc"]],
+      order: [[$(".order-by").index(), "asc"]], //this class is added to a <th> for the column we want to pre order.This needs an index integer so we are finding the index of the matching column
       columnDefs: [
         { targets: "no-sort", orderable: false }, //this class added to a <th> prevents sorting
         { targets: "hide-onload", visible: false }, //this class added to a <th> hides that column of data
