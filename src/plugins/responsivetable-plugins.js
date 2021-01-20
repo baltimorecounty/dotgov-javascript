1e156cc567625b6c3a3249ab94861c83bdd175cc
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-20 10:29:19 -0500

    Added table component to min file

--- src/plugins/responsivetable-plugins.js
+++ src/plugins/responsivetable-plugins.js
@@ -1,5 +1,10 @@
+/** All files required to make responsive table function */
+
+/** Responsive Table Includes */
+
 import "./jquery-3.5.1";
 import "./moment";
 import "./jquery.dataTables";
-import "./dataTables.responsive"
-import "./datetime-moment"
+import "./dataTables.responsive";
+import "./datetime-moment";
+import "../components/ResponsiveTable";
