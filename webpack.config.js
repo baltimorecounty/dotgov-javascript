26fd46449badbc1930813623185110cb19c1fea2
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-20 10:32:42 -0500

    path spelled correctly

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     ScrollToTop: "./src/components/ScrollToTop.js",
     PhoneDirectory: "./src/site/PhoneDirectory.js",
     ResponsiveTable: "./src/components/ResponsiveTable.js",
-    ResponsiveTablePlugins: "./src/plugin/ResponsiveTable-Plugins.js",
+    ResponsiveTablePlugins: "./src/plugins/ResponsiveTable-Plugins.js",
   },
   output: {
     path: path.join(__dirname, "dist"),
