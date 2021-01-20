fc5a38dda3f5227e7bb33fca5799388af2319cad
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-20 13:35:00 -0500

    updates to table includes

--- src/plugins/responsivetable-plugins.js
+++ src/plugins/responsivetable-plugins.js
@@ -2,9 +2,10 @@
 
 /** Responsive Table Includes */
 
-import "./jquery-3.5.1";
-import "./moment";
-import "./jquery.dataTables";
-import "./dataTables.responsive";
-import "./datetime-moment";
+fetch("https://code.jquery.com/jquery-3.5.1.js");
+fetch("https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.8.4/moment.min.js");
+fetch("https://cdn.datatables.net/plug-ins/1.10.22/sorting/datetime-moment.js");
+fetch("https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js");
+fetch("https://cdn.datatables.net/v/dt/dt-1.10.23/datatables.min.js");
+
 import "../components/ResponsiveTable";
