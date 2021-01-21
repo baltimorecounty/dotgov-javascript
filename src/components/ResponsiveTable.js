3eb241a308908f9450b4fea85846f94074954bc9
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-01-21 13:38:53 -0500

    IE 11 partially fixed

--- src/components/ResponsiveTable.js
+++ src/components/ResponsiveTable.js
@@ -9,7 +9,9 @@ load("https://code.jquery.com/jquery-3.5.1.js", function () {
         function () {
           load(
             "https://cdn.datatables.net/plug-ins/1.10.22/sorting/datetime-moment.js",
-            window.addEventListener("load", CreateDataTable)
+            function () {
+              CreateDataTable();
+            }
           );
         }
       );
@@ -17,43 +19,46 @@ load("https://code.jquery.com/jquery-3.5.1.js", function () {
   );
 });
 
-//Any table with a class of responsive-main-table will have this run against it to create the responsive data table
+//We are doing this crazy way of loading strictly because IE will not load the inclusions in the correct order. This gives it a chance to loadthe files before executing the datatable create function
 const CreateDataTable = () => {
-  //Windows variables that change the appearance of the datatable. If this doesn't exist then we default the values.
+  //Any table with a class of responsive-main-table will have this run against it to create the responsive data table
+  window.onload = new (function () {
+    //Windows variables that change the appearance of the datatable. If this doesn't exist then we default the values.
 
-  const isSearchable = window.responsivetable
-    ? window.responsivetable.isSearchable
-    : false;
-  const searchText = window.responsivetable
-    ? window.responsivetable.searchText
-    : "";
-  const placeHolderText = window.responsivetable
-    ? window.responsivetable.placeHolderText
-    : "";
+    const isSearchable = window.responsivetable
+      ? window.responsivetable.isSearchable
+      : false;
+    const searchText = window.responsivetable
+      ? window.responsivetable.searchText
+      : "";
+    const placeHolderText = window.responsivetable
+      ? window.responsivetable.placeHolderText
+      : "";
 
-  $.fn.dataTable.moment("MMMM D, YYYY"); //format of the date we want to recognize for sorting https://datatables.net/blog/2014-12-18
+    $.fn.dataTable.moment("MMMM D, YYYY"); //format of the date we want to recognize for sorting https://datatables.net/blog/2014-12-18
 
-  if (!$.fn.DataTable.isDataTable(".responsive-main-table")) {
-    $(".responsive-main-table").DataTable({
-      info: false,
-      paging: false,
-      bFilter: false,
-      processing: true,
-      searching: isSearchable,
-      language: {
-        searchPlaceholder: placeHolderText,
-        search: searchText,
-      },
-      ordering: true,
-      responsive: true,
-      autoWidth: false,
-      order: [[$(".order-by").index(), "asc"]], //this class is added to a <th> for the column we want to pre order.
-      //This needs an index integer so we are finding the index of the matching column.
-      //If no class is set then it defaults to 0 so first column
-      columnDefs: [
-        { targets: "no-sort", orderable: false }, //this class added to a <th> prevents sorting
-        { targets: "hide-onload", visible: false }, //this class added to a <th> hides that column of data
-      ],
-    });
-  }
+    if (!$.fn.DataTable.isDataTable(".responsive-main-table")) {
+      $(".responsive-main-table").DataTable({
+        info: false,
+        paging: false,
+        bFilter: false,
+        processing: true,
+        searching: isSearchable,
+        language: {
+          searchPlaceholder: placeHolderText,
+          search: searchText,
+        },
+        ordering: true,
+        responsive: true,
+        autoWidth: false,
+        order: [[$(".order-by").index(), "asc"]], //this class is added to a <th> for the column we want to pre order.
+        //This needs an index integer so we are finding the index of the matching column.
+        //If no class is set then it defaults to 0 so first column
+        columnDefs: [
+          { targets: "no-sort", orderable: false }, //this class added to a <th> prevents sorting
+          { targets: "hide-onload", visible: false }, //this class added to a <th> hides that column of data
+        ],
+      });
+    }
+  })();
 };
