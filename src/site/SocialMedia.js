e8574f187341dce5394afee4e756065986cc4988
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-02-19 12:56:56 -0500

    various bug fixes

--- src/site/SocialMedia.js
+++ src/site/SocialMedia.js
@@ -2,29 +2,33 @@
  * This handles the twitter social media button event
  * Generates an href for the current page
  */
-window.twttr = (function (d, s, id) {
-  var js,
-    fjs = d.getElementsByTagName(s)[0],
-    t = window.twttr || {};
-  if (d.getElementById(id)) return t;
-  js = d.createElement(s);
-  js.id = id;
-  js.src = "https://platform.twitter.com/widgets.js";
-  fjs.parentNode.insertBefore(js, fjs);
-  t._e = [];
-  t.ready = function (f) {
-    t._e.push(f);
-  };
-  return t;
-})(document, "script", "twitter-wjs");
-twttr.ready(function (twttr) {
-  // bind events here
-  var twitterButton = document.getElementById("twitterButton");
-  if (twitterButton) {
-    twitterButton.href += "&url=" + window.location.href;
-  }
-  var facebookButton = document.getElementById("facebookButton");
-  if (facebookButton) {
-    facebookButton.href += "?u=" + window.location.href;
-  }
-});
+function LoadTwitter() {
+  window.twttr = (function (d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0],
+      t = window.twttr || {};
+    if (d.getElementById(id)) return t;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = "https://platform.twitter.com/widgets.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    t._e = [];
+    t.ready = function (f) {
+      t._e.push(f);
+    };
+    return t;
+  })(document, "script", "twitter-wjs");
+  twttr.ready(function (twttr) {
+    // bind events here
+    var twitterButton = document.getElementById("twitterButton");
+    if (twitterButton) {
+      twitterButton.href += "&url=" + window.location.href;
+    }
+    var facebookButton = document.getElementById("facebookButton");
+    if (facebookButton) {
+      facebookButton.href += "?u=" + window.location.href;
+    }
+  });
+}
+
+window.addEventListener("load", LoadTwitter);
