3d50f0dd5c93f6600c206b473575252972fdce13
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-03-29 11:12:43 -0400

    added sticky header js

---
+++ src/utilities/stickyHeader.js
@@ -0,0 +1,19 @@
+window.onscroll = function () {
+  headerScroll();
+};
+
+var elementHeader = document.getElementsByClassName("dg_header");
+var elementMenu = document.getElementsByClassName("bc_site-nav__toggle-button");
+var header = elementHeader[0];
+var menu = elementMenu[0];
+var sticky = header.offsetTop;
+
+function headerScroll() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("dg_site-header-sticky");
+    menu.classList.add("bc_site-nav__toggle-button-sticky");
+  } else {
+    header.classList.remove("dg_site-header-sticky");
+    menu.classList.remove("bc_site-nav__toggle-button-sticky");
+  }
+}
