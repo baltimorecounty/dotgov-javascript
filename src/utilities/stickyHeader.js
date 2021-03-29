ebc48280fe082fe22f5392ba50b304ee6b2df7ec
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-03-29 14:34:14 -0400

    updates to sticky

--- src/utilities/stickyHeader.js
+++ src/utilities/stickyHeader.js
@@ -9,11 +9,18 @@ var menu = elementMenu[0];
 var sticky = header.offsetTop;
 
 function headerScroll() {
+  var ua = window.navigator.userAgent;
+  var msie = ua.indexOf("MSIE ");
+
   if (window.pageYOffset > sticky) {
-    header.classList.add("dg_site-header-sticky");
+    msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) //if a boomer is using IE use a different class that makes IE not suck
+      ? header.classList.add("dg_site-header-sticky-IE")
+      : header.classList.add("dg_site-header-sticky");
     menu.classList.add("bc_site-nav__toggle-button-sticky");
   } else {
-    header.classList.remove("dg_site-header-sticky");
+    msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)
+      ? header.classList.remove("dg_site-header-sticky-IE")
+      : header.classList.remove("dg_site-header-sticky");
     menu.classList.remove("bc_site-nav__toggle-button-sticky");
   }
 }
