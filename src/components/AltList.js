02ec2cfdc1a398f33db8ec074a416539cef88c5c
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-04-19 13:41:31 -0400

    comments

--- src/components/AltList.js
+++ src/components/AltList.js
@@ -2,15 +2,15 @@ const findCurrentRelatedLink = () => {
   var list = document.getElementsByClassName("dg_alt-list");
   var url = window.location.href;
   var index = url.indexOf("?");
-  //We use google translate which adds a google extension at the end of the url if in a different language. This makes sure that the correct alr
-  //list item is still highlighted if google is translating and also ensures the footer is updated as well.
+  //We use google translate which adds a google extension at the end of the url if in a different language. This makes sure that the correct
+  //alt list item is still highlighted if google is translating and also ensures the footer is updated as well.
   var urlTranslate = url.substring(index);
   var urlNoTranslate = url.replace(urlTranslate, "");
 
   Array.prototype.forEach.call(list, (item) => {
     var listItem = item.getElementsByTagName("a");
     Array.prototype.forEach.call(listItem, (x) => {
-      if (x.href === urlNoTranslate || x.href === url) { //If the translate option is removed and urls still match then flag it, if the footer url matches current url flag it.
+      if (x.href === urlNoTranslate || x.href === url) {
         x.parentNode.className = "dg_alt-list-current";
       }
     });
