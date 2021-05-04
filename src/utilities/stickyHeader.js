54f77b7a76f9b928dbd67d446f6955b2b4f391dc
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-05-04 08:32:37 -0400

    updates to JS sticky header

--- src/utilities/stickyHeader.js
+++ src/utilities/stickyHeader.js
@@ -9,18 +9,12 @@ var menu = elementMenu[0];
 var sticky = header.offsetTop;
 
 function headerScroll() {
-  var ua = window.navigator.userAgent;
-  var msie = ua.indexOf("MSIE ");
-
+  console.log("MObile scroll is working");
   if (window.pageYOffset > sticky) {
-    msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./) //if a boomer is using IE use a different class that makes IE not suck
-      ? header.classList.add("dg_site-header-sticky-IE")
-      : header.classList.add("dg_site-header-sticky");
+    header.classList.add("dg_site-header-sticky");
     menu.classList.add("bc_site-nav__toggle-button-sticky");
   } else {
-    msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)
-      ? header.classList.remove("dg_site-header-sticky-IE")
-      : header.classList.remove("dg_site-header-sticky");
+    header.classList.remove("dg_site-header-sticky");
     menu.classList.remove("bc_site-nav__toggle-button-sticky");
   }
 }
