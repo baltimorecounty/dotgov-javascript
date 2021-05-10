20f31ceaab9d12020e7a5e4c64ef9f359ad72130
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-05-10 10:45:47 -0400

    updates to sticky

--- src/site/closing.js
+++ src/site/closing.js
@@ -257,4 +257,3 @@ var getTodaysDate = function () {
     $closingsTable.show();
   });
 })(jQuery);
-
--- src/utilities/stickyHeader.js
+++ src/utilities/stickyHeader.js
@@ -1,15 +1,12 @@
 window.onscroll = function () {
-  headerScroll();
-};
+  var elementHeader = document.getElementsByClassName("dg_header");
+  var elementMenu = document.getElementsByClassName(
+    "bc_site-nav__toggle-button"
+  );
+  var header = elementHeader[0];
+  var menu = elementMenu[0];
+  var sticky = header.offsetTop;
 
-var elementHeader = document.getElementsByClassName("dg_header");
-var elementMenu = document.getElementsByClassName("bc_site-nav__toggle-button");
-var header = elementHeader[0];
-var menu = elementMenu[0];
-var sticky = header.offsetTop;
-
-function headerScroll() {
-  console.log("MObile scroll is working");
   if (window.pageYOffset > sticky) {
     header.classList.add("dg_site-header-sticky");
     menu.classList.add("bc_site-nav__toggle-button-sticky");
@@ -17,4 +14,4 @@ function headerScroll() {
     header.classList.remove("dg_site-header-sticky");
     menu.classList.remove("bc_site-nav__toggle-button-sticky");
   }
-}
+};
