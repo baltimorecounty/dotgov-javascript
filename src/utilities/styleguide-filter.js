8151ddd0aeb9edb807589b37b1fd9835efd4cf0c
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-06-03 10:22:09 -0400

    updates to responsive table sort order

--- src/utilities/styleguide-filter.js
+++ src/utilities/styleguide-filter.js
@@ -1,12 +1,16 @@
 function filterStyleGuide(input) {
   var parentDiv = document.getElementsByClassName("styleguide-left-navigation");
+  var header = parentDiv[0].getElementsByTagName("h3");
   var lists = parentDiv[0].getElementsByTagName("ul");
-  var inputFilter = document.getElementById("");
 
   for (var i = 0; i < lists.length; i++) {
     var listItems = lists[i].getElementsByTagName("li");
     for (var j = 0; j < listItems.length; j++) {
-      if (listItems[j].innerHTML.toLowerCase().indexOf(input.value.toLowerCase().trim()) > -1) {
+      if (
+        listItems[j].innerHTML
+          .toLowerCase()
+          .indexOf(input.value.toLowerCase().trim()) > -1
+      ) {
         listItems[j].style.display = "";
       } else {
         listItems[j].style.display = "none";
