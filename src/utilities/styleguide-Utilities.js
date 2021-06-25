3aef0a0031af4b4742a6bd5179c7a704feae5ee5
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-06-25 15:09:59 -0400

    Added auto collpase for search

--- src/utilities/styleguide-Utilities.js
+++ src/utilities/styleguide-Utilities.js
@@ -2,22 +2,39 @@
 //Style Guide Filter search box
 function filterStyleGuide(input) {
   var parentDiv = document.getElementsByClassName("styleguide-left-navigation");
-  var header = parentDiv[0].getElementsByTagName("h3");
+  var accordion = parentDiv[0].getElementsByClassName("dg_accordion");
   var lists = parentDiv[0].getElementsByTagName("ul");
 
+  let itemCount = 0;
   for (var i = 0; i < lists.length; i++) {
-    var listItems = lists[i].getElementsByTagName("li");
-    for (var j = 0; j < listItems.length; j++) {
-      if (
-        listItems[j].innerHTML
-          .toLowerCase()
-          .indexOf(input.value.toLowerCase().trim()) > -1
-      ) {
-        listItems[j].style.display = "";
+    if (!lists[i].classList.contains("styleguide-nav")) {
+      var listItems = lists[i].getElementsByTagName("li");
+
+      for (var j = 0; j < listItems.length; j++) {
+        if (
+          listItems[j].innerHTML
+            .toLowerCase()
+            .indexOf(input.value.toLowerCase().trim()) > -1
+        ) {
+          listItems[j].style.display = "";
+          itemCount = +1;
+        } else {
+          listItems[j].style.display = "none";
+        }
+      }
+
+      var accordion = lists[i].closest(".dg_accordion__collapsible");
+      var listDiv = lists[i].closest(".collapse ");
+
+      if (itemCount > 0) {
+        accordion.classList.remove("collapsed");
+        listDiv.classList.add("show");
       } else {
-        listItems[j].style.display = "none";
+        accordion.classList.add("collapsed");
+        listDiv.classList.remove("show");
       }
     }
+    itemCount = 0;
   }
 }
 //*************************************************************** */
