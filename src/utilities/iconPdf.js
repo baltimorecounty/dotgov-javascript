d27adf6438a79a44e957d3692c845b7f9676ef95
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2021-07-08 10:08:17 -0400

    clicking header when menu opens closes menu

--- src/utilities/iconPdf.js
+++ src/utilities/iconPdf.js
@@ -1,20 +1,38 @@
 function AddIconToPdf() {
-  var anchors = document.getElementsByTagName("a");
+  var anchors = [].slice.call(document.getElementsByTagName("a"));
+  var footer = document.getElementsByTagName("footer");
 
-  for (var i = 0; i < anchors.length; i++) {
+  var footerAnchors = [];
+  footerAnchors = [].slice.call(footer[0].getElementsByTagName("a"));
+
+  for (var i = 0; i < footer.length; i++) {
+    var elements = footer[i].getElementsByTagName("a");
+    var items = [].slice.call(elements);
+    items.forEach((x) => footerAnchors.push(x));
+  }
+
+  let neededElements = anchors.filter((item) => !footerAnchors.includes(item));
+
+  for (var i = 0; i < neededElements.length; i++) {
     var imgPDF = new Image();
-    var imgExternal = new Image();
+    imgPDF.classList.add("dg_external-link-image");
+    imgPDF.alt = "pdf";
     imgPDF.src =
-      "https://www.adobe.com/content/dam/cc/en/legal/images/badges/PDF_32.png";
+      "https://dev.baltimorecountymd.gov/sebin/n/n/icon-pdf-solid.png";
+
+    var imgExternal = new Image();
+    imgExternal.alt = "external link";
     imgExternal.src =
       "https://www.baltimorecountymd.gov/sebin/j/r/icon-link-image.png";
-    imgPDF.alt = "pdf";
-    imgPDF.click()
-    if (anchors[i].href != "") {
-      if (anchors[i].href.indexOf(".pdf") > -1) {
-        anchors[i].after(imgPDF);
-      } else if (anchors[i].href.indexOf("baltimorecountymd") === -1) {
-        anchors[i].after(imgExternal);
+    imgExternal.classList.add("dg_external-link-image");
+
+    if (neededElements[i].href != "") {
+      if (neededElements[i].href.indexOf(".pdf") > -1) {
+        neededElements[i].after(imgPDF);
+      } else if (neededElements[i].href.indexOf("baltimorecountymd") === -1) {
+        neededElements[i].after(imgExternal);
+      } else if (neededElements[i].href.indexOf("webex") > -1) {
+        neededElements[i].after(imgExternal);
       }
     }
   }
