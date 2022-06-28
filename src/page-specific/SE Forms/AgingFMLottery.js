f73b62aea0f340edbf817524a701df20ea10f888
Timothy McCoy <tmccoy@baltimorecountymd.gov>
2022-06-28 08:25:17 -0400

    Aging Lottery form validation complete

--- src/page-specific/SE Forms/AgingFMLottery.js
+++ src/page-specific/SE Forms/AgingFMLottery.js
@@ -5,9 +5,37 @@ function onSubmit(token) {
   for (var i = 0; i < requiredElements.length; i++) {
     if (!requiredElements[i].value.trim()) {
       var fieldText = $("label[for='" + requiredElements[i].id + "']").text();
+      fieldText = fieldText.replace(">>", "");
 
       alertMessage += "Please enter a value for " + fieldText + "\n";
     }
+  }
+
+  //******************************************************************
+  //This is a lesson in what NOT to do when finding elements on a page
+  //If SE changes the naming convention this no longer works. But I guess
+  //that could happen with structure as well. Anyway we are finding the
+  //groups of inputs and doing a count on how many are checked in each
+  //group. If we get a count less then 1 we flag an error.
+  //******************************************************************
+  var Ethnicity = document.querySelectorAll(
+    'input[name="fieldName16"]:checked'
+  );
+  var Race = document.querySelectorAll('input[name="fieldName18"]:checked');
+  var Acknowledge = document.querySelectorAll(
+    'input[name="fieldName17"]:checked'
+  );
+
+  if (Ethnicity.length < 1) {
+    alertMessage += "Please select a value for Ethnicity" + "\n";
+  }
+
+  if (Race.length < 1) {
+    alertMessage += "Please select a value for Race" + "\n";
+  }
+
+  if (Acknowledge.length < 1) {
+    alertMessage += "Please select a value for Acknowledge" + "\n";
   }
 
   if (alertMessage === "") {
