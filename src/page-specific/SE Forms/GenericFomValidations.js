0410369b0bfa1f72c6a6f2653d2e426fb2cc9cd5
Timothy McCoy <tmccoy@baltimorecountymd.gov>
2022-06-29 08:57:48 -0400

    Added a Generic Form Template

---
+++ src/page-specific/SE Forms/GenericFomValidations.js
@@ -0,0 +1,61 @@
+function onSubmit() {
+  var requiredElementstext = document.querySelectorAll(
+    'input[type="text"].seRequiredElement'
+  );
+  var requiredElementsCheckbox = document.querySelectorAll(
+    'input[type="checkbox"].seRequiredElement'
+  );
+  var requiredElementsRadio = document.querySelectorAll(
+    'input[type="radio"].seRequiredElement'
+  );
+
+  var alertMessage = "";
+
+  for (var i = 0; i < requiredElementstext.length; i++) {
+    if (!requiredElementstext[i].value.trim()) {
+      var fieldText = $(
+        "label[for='" + requiredElementstext[i].id + "']"
+      ).text();
+      fieldText = fieldText.replace(">>", "");
+
+      alertMessage += "Please enter a value for " + fieldText + "\n";
+    }
+  }
+
+  alertMessage += checkButtonInputs(requiredElementsCheckbox);
+  alertMessage += checkButtonInputs(requiredElementsRadio);
+
+  if (alertMessage === "") {
+    document.getElementById("agingFMlottery").submit();
+  } else {
+    alert(alertMessage);
+  }
+}
+
+function checkButtonInputs(buttonSet) {
+  var dict = {};
+  buttonSet.forEach((obj) => {
+    dict[obj.name] = (dict[obj.name] || []).concat([obj]);
+  });
+
+  var group = Object.keys(dict).map((k) => dict[k]);
+
+  var alertMessage = "";
+
+  for (var i = 0; i < group.length; i++) {
+    var count = group[i].filter(function (item) {
+      return item.checked;
+    }).length;
+
+    if (count === 0) {
+      var labelElement = document.getElementById(group[i][0].id);
+
+      var ParentDiv = labelElement.closest(".SEAFWrapper");
+      var ChildLabel = ParentDiv.querySelector("div.seLabelCell");
+      var fieldText = ChildLabel.innerHTML;
+      fieldText = fieldText.split("<")[0];
+      alertMessage += "Please select a value for " + fieldText + "\n";
+    }
+  }
+  return alertMessage ? alertMessage : "";
+}
