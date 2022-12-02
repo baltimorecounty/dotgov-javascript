cf8ad358135df1d1abfc2ef29ec6f1a6a8ab2283
Brody Barbour <107436068+bbarbourbcmd@users.noreply.github.com>
2022-12-02 15:34:48 -0500

    Traps focus within dropdown when tabbing translate dropdown, fixes typos, cleans up unneeded markup

--- src/site/translateButton.js
+++ src/site/translateButton.js
@@ -9,7 +9,9 @@ var keys = {
 
 var toggleButton = $('.dropdown-toggle');
 var dropdownMenu = $('.language-dropdown-menu');
-var dropdownWrapper = $('.language-dropdown')
+var dropdownMenuDesktop = dropdownMenu.not('.language-dropdown--mobile .language-dropdown-menu');
+var dropdownMenuMobile = $('.language-dropdown--mobile');
+var dropdownWrapper = $('.language-dropdown');
 
 $(function() {
 
@@ -17,7 +19,9 @@ $(function() {
         $(dropdownMenu).slideToggle(function() {
             if ($(dropdownMenu).is(':visible')) {
                 $(toggleButton).attr('aria-expanded','true');
-                $(dropdownMenu).focus();
+                $(dropdownMenu).focus().attr('tabindex', '-1').on('keydown', function(event){ //force focus
+                    handleFocusTrap(event);
+                });
             } else {
                 $(toggleButton).attr('aria-expanded', 'false');
             }
@@ -33,7 +37,7 @@ $(function() {
                     $(dropdownMenu).slideUp();
                     $(toggleButton).attr('aria-expanded','false');
                 }
-        }      
+        }
     });
 
     $(dropdownMenu).on('keydown', function(event) {
@@ -56,81 +60,97 @@ $(function() {
 
 }); //end ready function
 
+//22750-translate-tabbing
+function handleFocusTrap(event) {
+    //check if event is a tab - if not a tab, return.
+    if (event.key.toLowerCase() !== 'tab') {
+        return;
+    }
+
+    var tabbable;
+
+    //Split into 2 possible variables to account for different markup for mobile and desktop
+    //grab all href content that does not have a tabindex of -1 and add into content we can tab through
+    if (window.matchMedia("(max-width: 768px)").matches) {
+        // if screen size is lower than tablet size, grab tabbable items from mobile menu.
+        tabbable = $().add(dropdownMenuMobile.find('[href]')).add(dropdownMenuMobile.find('[tabindex]:not([tabindex="-1"])'));
+    } else {
+        // if screen size is higher than tablet size, grab tabbable items from desktop menu.
+        tabbable = $().add(dropdownMenuDesktop.find('[href]')).add(dropdownMenuDesktop.find('[tabindex]:not([tabindex="-1"])'));
+    }
+
+    // listen store target event is performed on.
+    var target = $(event.target);
+
+    // if event contains the shift key, check if we're at the first item in the tabbable list so we can loop back to the last item in the list.
+    if (event.shiftKey) {
+        if (target.is(dropdownMenuDesktop) || target.is(tabbable.first())) {
+            event.preventDefault();
+            tabbable.last().focus();
+        }
+    } else {
+        // if NOT holding down the shift button, check to see if we are currently on the last tabbable item, and go to the first tabbable item in the list.
+        if (target.is(tabbable.last())) {
+            event.preventDefault();
+            tabbable.first().focus();
+        }
+    }
+}
+
 // Allow keyboard navigation for accessibility
 function doKeypress(keys, event) {
-    current = $(':focus');
-    current.addClass('highlight');
 
     switch (event.which) {
-        
+
         case keys.ESC:
             $(dropdownMenu).slideUp();
             $(toggleButton).trigger('focus');
             $(toggleButton).attr('aria-expanded','false');
             break;
 
-        case keys.TAB:
-            if (current.hasClass('last')) {
-                if (event.shiftKey) {
-                    current.removeClass('highlight').prev('a').addClass('highlight').focus();
-                } else {
-                    $(toggleButton).attr('aria-expanded','false');
-                    $(dropdownMenu).slideUp();
-                    $('*').removeClass('highlight');
-                }
-            }
-
-            if (event.shiftKey) {
-                current.removeClass('highlight').prev('a').addClass('highlight').focus();
-            } else {
-                current.removeClass('highlight').next('a').addClass('highlight').focus();
-            }
-
-            break;
-        
         case keys.UP:
             event.preventDefault();
             event.stopPropagation();
-            return moveUp(highligted);
+            return moveUp(highlighted);
 
         case keys.DOWN:
-            highligted = $(':focus');
+            highlighted = $(':focus');
             event.preventDefault();
             event.stopPropagation();
-            return moveDown(highligted);
+            return moveDown(highlighted);
 
-        
+
         case keys.RETURN:
-            highligted = $(':focus');
-            if (highligted) {
-                if (highligted.attr('id') === 'language-dropdown-menu' || highlighted === $(toggleButton)) {
+            highlighted = $(':focus');
+            if (highlighted) {
+                if (highlighted.attr('id') === 'language-dropdown-menu' || highlighted === $(toggleButton)) {
                     return false;
                 }
                 break;
             }
 
         case keys.SPACE:
-            highligted = $('.highlight');
-            if (highligted) {
-                if (highligted.attr('id') === 'language-dropdown-menu') {
+            highlighted = $('.highlight');
+            if (highlighted) {
+                if (highlighted.attr('id') === 'language-dropdown-menu') {
                     return false;
-                } else {					  
+                } else {
                     event.preventDefault();
                     event.stopPropagation();
-                    return selectOption(highligted);
+                    return selectOption(highlighted);
                 }
                 break;
             }
-        
+
         default:
             return;
     }
 } //doKeypress function
 
-function selectOption(highligted) {
-    if (highligted) {
-        highligted.attr('aria-selected', true);
-        $(toggleButton).html(highligted.text());
+function selectOption(highlighted) {
+    if (highlighted) {
+        highlighted.attr('aria-selected', true);
+        $(toggleButton).html(highlighted.text());
         $(toggleButton).attr('aria-expanded','false');
         $(dropdownMenu).slideUp();
         $('*').removeClass('highlight');
@@ -139,10 +159,9 @@ function selectOption(highligted) {
     }
 } //selectOption Function
 
-
 function moveDown(highligted) {
     highligted = $(':focus');
-    
+
     if (highligted.attr('id') === 'language-dropdown-menu') {
         highligted.removeClass('highlight');
         highligted.children().first('li').children('a').addClass('highlight').focus();
